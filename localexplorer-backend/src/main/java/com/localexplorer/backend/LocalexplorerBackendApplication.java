6be907fa8fe7b182eb1adb247a48374f2d582f28
josevbrito <josevictorbritoc@gmail.com>
2025-07-03 00:02:06 -0300

    feat: Add initial backend and frontend project structures

---
+++ localexplorer-backend/src/main/java/com/localexplorer/backend/LocalexplorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localexplorer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalexplorerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalexplorerBackendApplication.class, args);
+	}
+
+}
